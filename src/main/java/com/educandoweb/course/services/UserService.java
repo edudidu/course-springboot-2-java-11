5b8754fd86830a4cb07a332369297c6154c59263
Eduardo <edu81jp@outlook.com>
2021-08-11 21:00:07 -0300

    Service layer, component registration

---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,29 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Service /*Registra userservice como componente do spring ai se pode usar o Autowired para injetar a dependencia automaticametne8 poderia ser @component tbm*/
+public class UserService {
+
+	@Autowired /*injeção de dependências*/
+	private UserRepository repository;
+	
+	public List<User> findAll () {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		
+     Optional<User> obj = repository.findById(id);		
+     return obj.get();
+    
+	}
+	
+}
